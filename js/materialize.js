f52578a8802ea05372bc38376ba64257e062947d
Brandon Chong <bschong2@illinois.edu>
2016-02-06 00:16:14 -0600

    Added some ui that need to be improved

--- js/materialize.js
+++ js/materialize.js
@@ -1,4 +1,5 @@
 // Turns a basic layout page into cs241's material design layout
+
 (function () {
   // Global Variables
   var elements = $("#content").children()
@@ -18,18 +19,21 @@
   sections.push($(elements.slice(last_index,elements.length)))
 
   // Style the sections
-  for (var i = 0; i < sections.length; i++) {
-    // For each section apply classes and wrap in divs
-    var section = sections[i]
+  var makeCard = function (section){
     $(section)
       // .append("<div class='mdl-card__actions mdl-card--border'></div>")
       .wrapAll("<div class='pad'/>")
       .wrapAll("<div class='mdl-card mdl-shadow--2dp large-card'/>")
     var title = $(section[0])
     title
-      .addClass("mdl-card__title-text").css( "color", "white" )
+      .css( "color", "white" )
+      .addClass("mdl-card__title-text")
       .wrapAll("<div class='mdl-card__title' style='background-color: #03a9f4'/>")
     var text = $(section.slice(1,section.length))
     text.wrapAll("<div class='content'/>")
   }
+  sections.map(makeCard)
+
+  // Make all the headers inside a card the right size
+  $(".mdl-card :header").addClass("mdl-card__title-text")
 })()
--- js/toc.js
+++ js/toc.js
@@ -15,14 +15,32 @@
       headerIDs.push(elem.attr('id'))
     }
   }
-  
+
   // Generate Table of Content
   var toc = $("<ul class='toc'><h4>Contents</h4></ul>")
   for (var i = 0; i < headers.length; i++) {
     var header = headers[i]
     var headerID = headerIDs[i]
-    toc.append("<a href='#" + headerID + "'>" + header + "</a>")
+    toc.append("<a id='" + "toc_" + headerID + "' href='#" + headerID + "'>" + header + "</a>")
   }
 
   $("#toc").append(toc)
+
+  // function isScrolledIntoView(elem) {
+  //   http://stackoverflow.com/questions/487073/check-if-element-is-visible-after-scrolling
+  //   var $elem = $(elem);
+  //   var $window = $(window);
+  //
+  //   var docViewTop = $window.scrollTop();
+  //   var docViewBottom = docViewTop + $window.height();
+  //
+  //   var elemTop = $elem.offset().top;
+  //   var elemBottom = elemTop + $elem.height();
+  //
+  //   return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+  // }
+  //
+  // function highlightTemporary(elem) {
+  //   $(elem).addClass('highlight').delay(800).removeClass('highlight');
+  // }
 })()
