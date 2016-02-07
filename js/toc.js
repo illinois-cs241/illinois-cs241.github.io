1f1becb93ca036f7854fe4955e5cd27d895c8173
Brandon Chong <bschong2@illinois.edu>
2016-02-07 05:32:32 -0600

    Added code to highlight roc

--- js/toc.js
+++ js/toc.js
@@ -22,11 +22,39 @@
     var header = headers[i]
     var headerID = headerIDs[i]
     var link = $("<a id='" + "toc_" + headerID + "' href='#" + headerID + "'>" + header + "</a>")
-    // link.click(function() {
-    //   $("html,body").animate({scrollTop:$("#" + headerID).offset().top},"1000")
-    // })
     toc.append(link)
   }
 
   $("#toc").append(toc)
+
+  // Determines if an element is atleast partially in the viewport
+  function isInView(elem) {
+    // http://stackoverflow.com/questions/487073/check-if-element-is-visible-after-scrolling
+    var $elem = $(elem)
+    var $window = $(window)
+
+    var docViewTop = $window.scrollTop()
+    var docViewBottom = docViewTop + $window.height()
+
+    var elemTop = $elem.offset().top
+    var elemBottom = elemTop + $elem.height()
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop))
+      || ((elemBottom >= docViewTop) && (elemTop <= docViewBottom))
+      || ((elemTop <= docViewBottom) && (elemBottom >= docViewTop))
+  }
+
+  // Marks the entries in the toc that have their section visable
+  var headers = $( "div" ).children( "h2" )
+  $(".mdl-layout__content").scroll(function(){
+    for(var i = 0; i < headers.length; i++) {
+      var header = $(headers[i])
+      var tocEntry = $("#toc_"+header.attr('id'))
+      tocEntry.removeClass('inView')
+      var card = header.parent().parent()
+      if (isInView(card)){
+        tocEntry.addClass('inView')
+      }
+    }
+  })
 })()
