3d80765de5ba45d24f7821bdb4ee827295f22f77
Bhuvan Venkatesh <bhuvan.venkatesh21@gmail.com>
2017-01-05 19:55:28 -0600

    Restored mistakenly deleted part of script

--- js/toc.js
+++ js/toc.js
@@ -1,6 +1,21 @@
 // Adds a table of content to a page
 (function () {
   "use strict";
+  function isInView(elem) {   
+     // http://stackoverflow.com/questions/487073/check-if-element-is-visible-after-scrolling    
+     var $elem = $(elem)   
+     var $window = $(window)   
+     
+     var docViewTop = $window.scrollTop()    
+     var docViewBottom = docViewTop + $window.height()   
+     
+     var elemTop = $elem.offset().top    
+     var elemBottom = elemTop + $elem.height()   
+     
+     return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop))   
+       || ((elemBottom >= docViewTop) && (elemTop <= docViewBottom))   
+       || ((elemTop <= docViewBottom) && (elemBottom >= docViewTop))   
+   }   
 
   // Marks the entries in the toc that have their section visible
   var headers = $( "div" ).children( "h2" );
