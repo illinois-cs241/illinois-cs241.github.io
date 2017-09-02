be7889f7bd9cac427704dd98305f7e4ddade82c3
Ben Kurtovic <ben.kurtovic@gmail.com>
2017-09-02 02:36:43 -0500

    flip conditional

--- _scripts/update_wikibook.sh
+++ _scripts/update_wikibook.sh
@@ -13,7 +13,7 @@ fi
 git submodule foreach "git pull origin master"
 git add $WIKIBOOK
 
-if git diff-index --quiet --cached HEAD -- $WIKIBOOK; then
+if ! git diff-index --quiet --cached HEAD -- $WIKIBOOK; then
     COMMIT=$(git ls-tree --abbrev=10 HEAD $WIKIBOOK | awk '{print $3}')
     git commit -m "Updating wikibook to latest revision ($COMMIT)"
     git push origin develop
