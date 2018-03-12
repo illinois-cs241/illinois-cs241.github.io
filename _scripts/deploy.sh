60cf5176074d42066978e60f3a738ba1985a7293
Bhuvan Venkatesh <bhuvan.venkatesh21@gmail.com>
2018-03-11 21:44:14 -0500

    again

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+echo "Moving";
 mv ./_site ..;
+echo "Checking out";
 git checkout master;
+echo "Removing";
 rm -rf *;
+echo "Copying";
 cp -R ../_site/* .;
+ls;
+echo "CNAME";
 echo "cs241.cs.illinois.edu" > CNAME;
+echo "Adding";
 git add -A;
 git commit -m "Deploying from $(date -u +"%Y-%m-%dT%H:%M:%SZ")";
 git push origin master --force;
