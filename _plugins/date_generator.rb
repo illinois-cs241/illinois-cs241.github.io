0128daa2e738b55745163ec71a21929d3d7b2e70
Bhuvan Venkatesh <bhuvan.venkatesh21@gmail.com>
2018-11-07 17:23:31 -0600

    Semester is generated automagically

---
+++ _plugins/date_generator.rb
@@ -0,0 +1,14 @@
+module Jekyll
+
+  class SemesterGenerator < Generator
+
+    def generate(site)
+        now = Time.new
+        sem_abbr = (now.month > 7) ? "fa" : "sp"
+
+        site.config['semester'] = sem_abbr + (now.year % 100).to_s
+    end
+
+  end
+
+end
