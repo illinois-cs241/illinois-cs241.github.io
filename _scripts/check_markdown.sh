1408a32bcc016d3f04d809d1d40b2eb00b393f85
Bhuvan Venkatesh <bhuvan.venkatesh21@gmail.com>
2018-11-14 17:22:22 -0600

    I hate bash

--- _scripts/check_markdown.sh
+++ _scripts/check_markdown.sh
@@ -3,6 +3,7 @@
 set -e
 trap exit INT;
 MATCHES="0"
+set -x
 for file in *md _docs/*md;
 do
     OUTPUT=`(pandoc -f markdown -t json -s $file | SUGGEST=1 _scripts/md_spell_check.py | pandoc -f json -t markdown -o /dev/null) 2>&1`
