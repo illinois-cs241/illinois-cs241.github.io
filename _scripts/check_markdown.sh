e485688be05dad3726641f58cac40265b0828494
Bhuvan Venkatesh <bhuvan.venkatesh21@gmail.com>
2018-11-14 17:34:58 -0600

    Once more with feeling

--- _scripts/check_markdown.sh
+++ _scripts/check_markdown.sh
@@ -3,11 +3,10 @@
 set -e
 trap exit INT;
 MATCHES="0"
-set -x
 
 for file in *md _docs/*md;
 do
-    OUTPUT=`(pandoc -f markdown -t json -s $file | SUGGEST=1 python3 _scripts/md_spell_check.py | pandoc -f json -t markdown -o /dev/null) 2>&1`
+    OUTPUT=`(pandoc -f markdown -t json $file | SUGGEST=1 python3 _scripts/md_spell_check.py | pandoc -f json -t markdown -o /dev/null) 2>&1`
     if [ ! -z "$OUTPUT" ]; then
         echo "$file"
         echo "======="
