89219d37b8f1dc57f381bdfdd64c5a11f5bb686c
Bhuvan Venkatesh <bhuvan.venkatesh21@gmail.com>
2018-11-17 15:31:16 -0600

    Will travis pick this build up. We may never know

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e;
+
 TMPDIR=`mktemp -d`
 echo "Working in $TMPDIR"
 cd $TMPDIR
@@ -8,6 +10,7 @@ git init;
 git checkout master;
 
 echo "Copying"
+ls $TRAVIS_BUILD_DIR/_site/
 cp -r $TRAVIS_BUILD_DIR/_site/* .
 
 echo "CNAME";
@@ -15,4 +18,7 @@ echo "cs241.cs.illinois.edu" > CNAME;
 git add -A;
 git commit -m "Deploying from $(date -u +"%Y-%m-%dT%H:%M:%SZ")";
 git remote add origin git@github.com:${TRAVIS_REPO_SLUG}.git
-git push origin master --force;
+ls
+git status
+cd ${TRAVIS_BUILD_DIR}
+#git push origin master --force;
