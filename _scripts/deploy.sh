57def11a577d374dcaac3b63e0717706a6be69ea
bhuvy <bhuvan.venkatesh21@gmail.com>
2019-02-22 12:30:00 -0600

    plx work

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -2,10 +2,13 @@
 
 set -e;
 
+# Part 1 push to github
+
 TMPDIR=`mktemp -d`
 echo "Working in $TMPDIR"
 cd $TMPDIR
 
+git config --global core.sshCommand "ssh -i /tmp/deploy_rsa -F /dev/null"
 git init;
 
 echo "Copying"
@@ -15,7 +18,11 @@ touch .nojekyll
 git add -A;
 git commit -m "Deploying from $(date -u +"%Y-%m-%dT%H:%M:%SZ")";
 git remote add origin git@github.com:${TRAVIS_REPO_SLUG}.git
-ls
 git push origin master --force;
 
 cd ${TRAVIS_BUILD_DIR}
+
+# Part 2, copy to a grader
+
+scp _site -i /tmp/deploy_grader $DEPLOY_GRADER_USER@$DEPLOY_GRADER_HOST:$DEPLOY_GRADER_PATH
+
