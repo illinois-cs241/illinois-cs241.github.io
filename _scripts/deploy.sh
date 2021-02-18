0b5ad794ce51a5bcb9aaa16b82d8fe79807af2f2
Eric Zhang <ezhang887@gmail.com>
2021-02-17 21:16:59 -0800

    Update travis specific enviornment variables

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -12,15 +12,15 @@ git config --global core.sshCommand "ssh -i /tmp/dual_deploy_key -F /dev/null"
 git init;
 
 echo "Copying"
-cp -r $TRAVIS_BUILD_DIR/_site/* .
+cp -r $GITHUB_WORKSPACE/_site/* .
 touch .nojekyll
 
 git add -A;
 git commit -m "Deploying from $(date -u +"%Y-%m-%dT%H:%M:%SZ")";
-git remote add origin git@github.com:${TRAVIS_REPO_SLUG}.git
+git remote add origin git@github.com:${GITHUB_REPOSITORY}.git
 git push origin master --force;
 
-cd ${TRAVIS_BUILD_DIR}
+cd ${GITHUB_WORKSPACE}
 
 # Part 2, copy to a grader
 set -x;
